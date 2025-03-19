24420687d0655235f3dc38ea1a5d43c13b80e1ab
unknown <2m.laclef@gmail.com>
2025-03-19 13:46:46 +0000

    ajou

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
+// Fonction pour connecter à MongoDB
 const connectDB = async () => {
   try {
+    // Connexion à la base de données
     await mongoose.connect(
-      "mongodb+srv://atlas-sample-dataset-load-67dac2347e93545ac1542888:KAM8z9t5bf5NqGf@cluster0.y838u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://atlas-sample-dataset-load-67dac2347e93545ac1542888:KAM8z9t5bf5NqGf@cluster0.y838u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB connecté");
   } catch (error) {
-    console.error("Erreur de connexion à MongoDB", error);
-    process.exit(1);
+    // Gestion des erreurs
+    console.error("Erreur de connexion à MongoDB :", error);
+    process.exit(1); // Quitte le processus en cas d'échec
   }
 };
 
+// Exportation de la fonction
 module.exports = connectDB;
