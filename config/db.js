2e506fff54ca43ed2d250415d8d0574a2698216c
unknown <2m.laclef@gmail.com>
2025-03-19 14:04:44 +0000

    ajou

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
     // Connexion à la base de données
     await mongoose.connect(
-      "mongodb+srv://atlas-sample-dataset-load-67dac2347e93545ac1542888:KAM8z9t5bf5NqGf@cluster0.y838u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://oumar:0987654321Aa@cluster0.y838u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB connecté");
   } catch (error) {
