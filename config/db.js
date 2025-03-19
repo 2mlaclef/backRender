7511d4f2e087efee5b8e51903fe10ac4cb954417
unknown <2m.laclef@gmail.com>
2025-03-19 13:42:44 +0000

    ajou

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://oumar:KAM8z9t5bf5NqGf@cluster0.mongodb.net/etudiants?retryWrites=true&w=majority",
+      "mongodb+srv://atlas-sample-dataset-load-67dac2347e93545ac1542888:KAM8z9t5bf5NqGf@cluster0.y838u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
